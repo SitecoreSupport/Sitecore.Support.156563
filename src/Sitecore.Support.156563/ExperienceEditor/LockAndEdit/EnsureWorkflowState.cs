28110277e94b805806ac7a47ec183555d8176dd6
ido <ido@WS-IDO1UA1>
2017-04-24 13:10:47 +0300

    Extended the fix - now also fixes locking in the final workflow state

---
+++ src/Sitecore.Support.156563/ExperienceEditor/LockAndEdit/EnsureWorkflowState.cs
@@ -0,0 +1,42 @@
+﻿using Sitecore.Data.Items;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.LockAndEdit
+{
+  public class EnsureWorkflowState
+  {
+    private const string itemLockPath = "/-/speak/request/v1/expeditor/ExperienceEditor.LockItem";
+    protected void OnItemVersionCreated(object sender, System.EventArgs args)
+    {
+      if (System.Web.HttpContext.Current.Request.Url.AbsolutePath.ToLowerInvariant().Equals(itemLockPath, StringComparison.InvariantCultureIgnoreCase))
+      {
+        if (args != null && args is Sitecore.Events.SitecoreEventArgs)
+        {
+          var explArgs = args as Sitecore.Events.SitecoreEventArgs;
+          if (explArgs.Parameters.Length > 0)
+          {
+            var item = explArgs.Parameters[0] as Sitecore.Data.Items.Item;
+            if (item != null && !string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.Workflow]))
+            {
+              if (string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.WorkflowState]))
+              {
+                var workflowState = item.Database.GetItem(item[Sitecore.FieldIDs.Workflow]);
+                if (workflowState != null)
+                {
+                  string initialState = workflowState["Initial state"];
+                  if (!string.IsNullOrWhiteSpace(initialState))
+                  {
+                    using (new EditContext(item, false, true))
+                    {
+                      item[Sitecore.FieldIDs.WorkflowState] = initialState;
+                    }
+                  }                  
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- src/Sitecore.Support.156563/ExperienceEditor/Speak/Ribbon/Requests/LockItem/ToggleLockRequest.cs
+++ src/Sitecore.Support.156563/ExperienceEditor/Speak/Ribbon/Requests/LockItem/ToggleLockRequest.cs
@@ -1,5 +1,4 @@
-﻿using Sitecore.Common;
-using Sitecore.Configuration;
+﻿using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Data.Managers;
@@ -7,18 +6,14 @@ using Sitecore.Diagnostics;
 using Sitecore.ExperienceEditor.Speak.Server.Contexts;
 using Sitecore.ExperienceEditor.Speak.Server.Requests;
 using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.Support.ExperienceEditor.Utilites;
 using Sitecore.Web;
-using Sitecore.Workflows;
 using System;
-using System.Reflection;
-using System.Web;
 
 namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
 {
   public class ToggleLockRequest : PipelineProcessorRequest<ItemContext>
   {
-    private static readonly BindingFlags _bFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-
     private void HandleVersionCreating(Item finalItem)
     {
       if (base.RequestContext.Item.Version.Number != finalItem.Version.Number)
@@ -65,7 +60,7 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
         return item;
       }
 
-      var _contextField = workflow.GetType().GetField("_context", _bFlags);
+      var _contextField = workflow.GetType().GetField("_context", IsApprovedHandler.BFlags);
       var _contextValue = (Sitecore.Context.ContextData)_contextField.GetValue(workflow);
       if (_contextValue.IsAdministrator)
       {
@@ -75,7 +70,7 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
       {
         return this.Lock(item);
       }
-      if (!workflow.HasWorkflow(item) && !workflow.HasDefaultWorkflow(item))
+      if (!IsApprovedHandler.HasWorkflow(workflow, item) && !workflow.HasDefaultWorkflow(item))
       {
         return this.Lock(item);
       }
@@ -102,85 +97,7 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
 
     protected virtual bool IsApproved(Sitecore.Workflows.WorkflowContext workflow, Item item)
     {
-      Error.AssertObject(item, "item");
-      IWorkflow _workflow = GetWorkflow(workflow, item);
-      if (_workflow != null)
-      {
-        return _workflow.IsApproved(item, null);
-      }
-      return true;
-    }
-
-    protected virtual IWorkflow GetWorkflow(Sitecore.Workflows.WorkflowContext workflow, Item item)
-    {
-      Error.AssertObject(item, "item");
-      if (IsWorkflowEnabled(workflow))
-      {
-        IWorkflowProvider workflowProvider = item.Database.WorkflowProvider;
-        if (workflowProvider != null)
-        {
-          return workflowProvider.GetWorkflow(item);
-        }
-      }
-      return null;
-    }
-
-    protected virtual bool IsWorkflowEnabled(Sitecore.Workflows.WorkflowContext workflow)
-    {
-      switch (Switcher<WorkflowContextState, WorkflowContextState>.CurrentValue)
-      {
-        case WorkflowContextState.Default:
-          {
-            var _contextField = workflow.GetType().GetField("_context", _bFlags);
-            var _contextValue = (Sitecore.Context.ContextData)_contextField.GetValue(workflow);
-            return ((_contextValue.Site != null) && (_contextValue.Site.EnableWorkflow || IsReferrerInEditMode()));
-          }
-        case WorkflowContextState.Disabled:
-          return false;
-      }
-      return true;
-    }
-
-    protected virtual bool IsReferrerInEditMode()
-    {
-      const string editModePattern = "mode=edit";
-      const string urlEditModePattern = "sc_mode=edit";
-      var referrer = HttpContext.Current.Request.UrlReferrer;
-      if (referrer != null && !string.IsNullOrWhiteSpace(referrer.Query))
-      {
-        string innerQuery = null;
-        bool res = IsInQuery(editModePattern, referrer.Query, out innerQuery);
-        if (res)
-        {
-          return res;
-        }
-        else if (!res && !string.IsNullOrWhiteSpace(innerQuery))
-        {
-          string innerQueryStub = null;
-          return IsInQuery(urlEditModePattern, innerQuery, out innerQueryStub);
-        }
-      }
-      return false;
-    }
-
-    protected virtual bool IsInQuery(string param, string query, out string innerQuery)
-    {
-      innerQuery = null;
-      var queryParts = query.Split(new char[] { '?', '&', '/' }, StringSplitOptions.RemoveEmptyEntries);
-      string urlParam = string.Empty;
-      foreach (string queryPart in queryParts)
-      {
-        string q = HttpContext.Current.Server.UrlDecode(queryPart);
-        if (param.Equals(q, StringComparison.InvariantCultureIgnoreCase))
-        {
-          return true;
-        }
-        if (q.StartsWith("url=", StringComparison.InvariantCultureIgnoreCase))
-        {
-          innerQuery = q;
-        }
-      }
-      return false;
+      return IsApprovedHandler.Process(workflow, item, null);
     }
   }
 }
