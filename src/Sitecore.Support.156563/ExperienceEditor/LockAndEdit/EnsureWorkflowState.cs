9744bc9338d2f6ed0a552b6859894fed65fd8b18
Andriy Shcherbak <andriy.shcherbak@sitecore.com>
2021-01-19 16:11:54 +0200

    Extend paths to check

--- src/Sitecore.Support.156563/ExperienceEditor/LockAndEdit/EnsureWorkflowState.cs
+++ src/Sitecore.Support.156563/ExperienceEditor/LockAndEdit/EnsureWorkflowState.cs
@@ -1,68 +1,81 @@
 ﻿using Sitecore.Data.Items;
 using Sitecore.Events;
 using System;
+using System.Collections.Generic;
 
 namespace Sitecore.Support.ExperienceEditor.LockAndEdit
 {
-  public class EnsureWorkflowState
-  {
-    private const string itemLockPath = "/-/speak/request/v1/expeditor/ExperienceEditor.LockItem";
-    protected void OnItemVersionCreated(object sender, System.EventArgs args)
+    public class EnsureWorkflowState
     {
-      Item item = null;
-      if (IsAbsolutePathEqual() && IsArgsValid(args, out item))
-      {
-        if (!string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.Workflow]))
+        private List<string> paths = new List<string>()
         {
-          if (string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.WorkflowState]))
-          {
-            var workflowState = item.Database.GetItem(item[Sitecore.FieldIDs.Workflow]);
-            if (workflowState != null)
+            "/-/speak/request/v1/expeditor/ExperienceEditor.LockItem",
+            "/-/speak/request/v1/expeditor/ExperienceEditor.Save.CallServerSavePipeline",
+            "/-/speak/request/v1/expeditor/ExperienceEditor.AddVersion.Execute"
+        };
+        protected void OnItemVersionCreated(object sender, System.EventArgs args)
+        {
+            Item item = null;
+            if (IsAbsolutePathEqual() && IsArgsValid(args, out item))
             {
-              string initialState = workflowState["Initial state"];
-              if (!string.IsNullOrWhiteSpace(initialState))
-              {
-                using (new EditContext(item, false, true))
+                if (!string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.Workflow]))
                 {
-                  item[Sitecore.FieldIDs.WorkflowState] = initialState;
+                    if (string.IsNullOrWhiteSpace(item[Sitecore.FieldIDs.WorkflowState]))
+                    {
+                        var workflowState = item.Database.GetItem(item[Sitecore.FieldIDs.Workflow]);
+                        if (workflowState != null)
+                        {
+                            string initialState = workflowState["Initial state"];
+                            if (!string.IsNullOrWhiteSpace(initialState))
+                            {
+                                using (new EditContext(item, false, true))
+                                {
+                                    item[Sitecore.FieldIDs.WorkflowState] = initialState;
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
-    }
 
-    protected virtual bool IsAbsolutePathEqual()
-    {
-      var context = System.Web.HttpContext.Current;
-      if (context != null && context.Request != null && context.Request.Url != null)
-      {
-        string path = context.Request.Url.AbsolutePath;
-        if (path != null)
+        protected virtual bool IsAbsolutePathEqual()
         {
-          return path.Equals(itemLockPath, StringComparison.InvariantCultureIgnoreCase);
+            var context = System.Web.HttpContext.Current;
+            if (context != null && context.Request != null && context.Request.Url != null)
+            {
+                string path = context.Request.Url.AbsolutePath;
+                if (path != null)
+                {
+                    foreach (var item in paths)
+                    {
+                        if (path.Equals(item, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+            return false;
         }
-      }
-      return false;
-    }
 
-    protected virtual bool IsArgsValid(System.EventArgs args, out Item item)
-    {
-      item = null;
-      if (args != null && args is SitecoreEventArgs)
-      {
-        var explArgs = args as SitecoreEventArgs;
-        if (explArgs.Parameters != null && explArgs.Parameters.Length > 0)
+        protected virtual bool IsArgsValid(System.EventArgs args, out Item item)
         {
-          item = explArgs.Parameters[0] as Item;
-          if (item != null && item.Database != null)
-          {
-            return true;
-          }
+            item = null;
+            if (args != null && args is SitecoreEventArgs)
+            {
+                var explArgs = args as SitecoreEventArgs;
+                if (explArgs.Parameters != null && explArgs.Parameters.Length > 0)
+                {
+                    item = explArgs.Parameters[0] as Item;
+                    if (item != null && item.Database != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
-      }
-      return false;
     }
-  }
 }
